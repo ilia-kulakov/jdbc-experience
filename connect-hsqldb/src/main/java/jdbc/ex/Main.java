71aadf77fbd0978e1b77e5c25236fabba4e15359
Ilia Kulakov <43149847+ilia-kulakov@users.noreply.github.com>
2022-12-15 22:12:04 +0100

    Read data from HSQLDB

--- connect-hsqldb/src/main/java/jdbc/ex/Main.java
+++ connect-hsqldb/src/main/java/jdbc/ex/Main.java
@@ -1,25 +1,28 @@
 package jdbc.ex;
 
+import jdbc.ex.tables.Tours;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     private static final String USERNAME = "dbuser";
     private static final String PASSWORD = "dbpassword";
-    private static final String CONN_STRING = "jdbc:hsqldb:file:data/explorecalifornia";
+    private static final String CONN_STRING = "jdbc:hsqldb:res:data/explorecalifornia";
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-            System.out.println("Connected");
+
+        try(
+            Connection connection = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM tours");
+            ) {
+            Tours.displayData(rs);
         } catch (SQLException e) {
             System.err.println(e);
-        } finally {
-            if(connection != null) {
-                connection.close();
-            }
         }
     }
 }
